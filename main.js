d0e3480fcabb948990549ce5fd0420ffe3484861
Lys <153897863+itsLys@users.noreply.github.com>
2024-02-05 09:18:24 +0100

    Add score keep and modal at the end of game

--- main.js
+++ main.js
@@ -2,99 +2,84 @@ function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
   let randomChoice = choices[(Math.floor(Math.random() * choices.length))];
   return randomChoice;
-} // Randomly chooses
-
+}
 const btnRock = document.querySelector('.player .rock');
 const btnPaper = document.querySelector('.player .paper');
 const btnScissors = document.querySelector('.player .scissors');
 const score = document.querySelector('.current-score');
-
+const roundStatus = document.querySelector('.game-status');
+const gameWinner = document.querySelector('.game-over>h1>span');
+const gameOver = document.querySelector('.modal-container');
+const startButton = document.querySelector('.start-button');
 
 let p = 0;
 let c = 0;
-score.textContent = ('PLAYER: ' + p + ' COMPUTER: ' + c);
-
-btnRock.addEventListener('click', () => {
-  if (playRound('rock') == 'loss') {
-    score.textContent = ('PLAYER: ' + p + ' COMPUTER: ' + ++c)
-  } else if (playRound('rock') == 'win') {
-    score.textContent = ('PLAYER: ' + ++p + ' COMPUTER: ' + c)
+function startGame() {
+  p = 0;
+  c = 0;
+  gameOver.style.display = '';
+  score.textContent = 'PLAYER: ' + p + ' COMPUTER: ' + c;
+  btnRock.addEventListener('click', (e) => {
+    e.preventDefault();
+    setScore(playRound('rock'));
+    console.log(p, c)
+  });
+  btnPaper.addEventListener('click', (e) => {
+    e.preventDefault();
+    setScore(playRound('paper'))
+  });
+  btnScissors.addEventListener('click', (e) => {
+    e.preventDefault();
+    setScore(playRound('scissors'))
+  });
+}
+function setScore(param) {
+  if (param === 'win') {
+    score.textContent = 'PLAYER: ' + ++p + ' COMPUTER: ' + c;
+    roundStatus.textContent = 'ROUND WIN';
+  } else if (param === 'loss') {
+    score.textContent = 'PLAYER: ' + p + ' COMPUTER: ' + ++c;
+    roundStatus.textContent = 'ROUND LOSS';
+  } else if (param === 'tie') {
+    roundStatus.textContent = 'IT\'S A TIE, TRY AGAIN';
+  };
+  if (score.textContent.includes('5')) {
+    gameOver.style.display = 'block';
+    finishGame();
   };
 
-});
-btnPaper.addEventListener('click', () => {
-  if (playRound('paper') == 'loss') {
-    score.textContent = ('PLAYER: ' + p + ' COMPUTER: ' + ++c)
-  } else if (playRound('rock') == 'win') {
-    score.textContent = ('PLAYER: ' + ++p + ' COMPUTER: ' + c)
+};
+function finishGame() {
+  if (score.textContent.includes('PLAYER: 5')) {
+    gameWinner.textContent = ' PLAYER WINS!';
+  } else if (score.textContent.includes('COMPUTER: 5')) {
+    gameWinner.textContent = ' COMPUTER WINS!';
   }
-});
-btnScissors.addEventListener('click', () => {
-  if (playRound('scissors') == 'loss') {
-    score.textContent = ('PLAYER: ' + p + ' COMPUTER: ' + ++c)
-  } else if (playRound('rock') == 'win') {
-    score.textContent = ('PLAYER: ' + ++p + ' COMPUTER: ' + c)
-  }
-})
-// if playround is loss, then c++
+};
 
-
-//Create a function the returns the choosen weapon
-//When btnRock is clicked, return string rock and so on
-
-function playRound(weapon) {
-  let playerSelection = weapon;
+//
+function playRound(playerSelection) {
   let computerSelection = getComputerChoice();
-  if ((playerSelection == 'rock')
-    && (computerSelection == 'paper')) {
-    return ('loss');
-  } else if ((playerSelection === 'paper')
-    && (computerSelection === 'scissors')) {
-    return ('loss');
-  } else if ((playerSelection === 'scissors')
-    && (computerSelection === 'rock')) {
-    return ('loss');
-  } else if ((playerSelection === 'paper')
-    && (computerSelection === 'rock')) {
-    return ('win');
-  } else if ((playerSelection === 'scissors')
-    && (computerSelection === 'paper')) {
-    return ('win');
-  } else if ((playerSelection === 'rock')
-    && (computerSelection === 'scissors')) {
-    return ('win');
-  } else if (playerSelection === computerSelection) {
+  if
+    (playerSelection === computerSelection) {
     return ('tie');
-  };
-}
-
-function playGame() {
-  let p = 0;
-  let c = 0;
-  // for (let i = 1; i < 6; i++) {
-  //   let round = playRound();
-  //   console.log('Round ' + i);
-  //   console.log(round);
-  //   if (round.includes('tie')) {
-  //     i--;
-  //     console.log('Try again!'); // Checks if the choice is valid or if it is a tie and replays the round
-  //   } else if (round.includes('Win')) {
-  //     p++;
-  //   }
-  //   else if (round.includes('Loose')) {
-  //     c++;
-  //   };
-  //   console.log('Current Score: ' + 'Player:' + p + ' AI:' + c);
-  //   if (p == 3 || c == 3) {
-  //     i = 6;
-  //   } //Checks if a score is 3 and ends the loop, concludes the game
-  // };
-  let winner;
-  if (p > c) {
-    winner = 'GAME OVER: YOU WON!'
+  } else if
+    (((playerSelection === 'rock') && (computerSelection === 'paper')) ||
+    ((playerSelection === 'paper') && (computerSelection === 'scissors')) ||
+    ((playerSelection === 'scissors') && (computerSelection === 'rock'))) {
+    return ('loss');
   }
-  else if (c > p) {
-    winner = 'GAME OVER: YOU LOST!'
+  else if
+    (((playerSelection === 'paper') && (computerSelection === 'rock')) ||
+    ((playerSelection === 'scissors') && (computerSelection === 'paper')) ||
+    ((playerSelection === 'rock') && (computerSelection === 'scissors'))) {
+    return ('win');
   };
-  console.log(winner);
-}
+};
+
+
+
+
+
+startButton.addEventListener('click', () => { document.location.reload() });
+startGame();
